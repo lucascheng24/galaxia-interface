adf2e98acfa62ab10f5f9f1b1efc1234005033d1
KelvinLi <limanchungofdragon@gmail.com>
2023-03-28 15:17:39 +0800

    add css file

--- src/auth/loginInterface/LoginInterface.tsx
+++ src/auth/loginInterface/LoginInterface.tsx
@@ -1,4 +1,5 @@
 import { Grid } from '@mui/material';
+import './LoginInterface.css';
 
 const LoginInterface = (props: any) => {
 
@@ -41,11 +42,6 @@ const LoginInterface = (props: any) => {
        });
   }
 
-
-  
-
-
-
   return (
     <Grid container className='common-PlainText'>
       <Grid item xs={8} className='common-border-01'>
@@ -65,62 +61,66 @@ const LoginInterface = (props: any) => {
         <div>Register</div>
       </Grid>
     </Grid>
-    // <Container>
-    //   <Row>
-    //     <Col sm={2}>
-    //       <div className='common-PlainText'>
-    //         {/* <in>
-    //         </Input> */}
-    //       </div>
-    //     </Col>
+    /* <Container>
+       <Row>
+         <Col sm={2}>
+           <div className='common-PlainText'>
+             {
+              // <in>
+              // <Input> 
+             } 
+           </div>
+         </Col>
 
-    //     <Col sm={6}>
-    //       <div className='common-PlainText'>col 2</div>
-    //     </Col>
+         <Col sm={6}>
+           <div className='common-PlainText'>col 2</div>
+         </Col>
 
-    //     <Col sm={4}>
-    //       <div className='common-PlainText'>col 3</div>
-    //     </Col>
-    //   </Row>
+         <Col sm={4}>
+           <div className='common-PlainText'>col 3</div>
+         </Col>
+       </Row>
 
-    // </Container>
-    // <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+     </Container>
+     <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
 
-    //   <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email'/>
-    //   <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password'/>
+       <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email'/>
+       <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password'/>
 
-    //   <div className="d-flex justify-content-between mx-3 mb-4">
-    //     <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-    //     <a href="!#">Forgot password?</a>
-    //   </div>
+       <div className="d-flex justify-content-between mx-3 mb-4">
+         <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+         <a href="!#">Forgot password?</a>
+       </div>
 
-    //   <MDBBtn className="mb-4">Sign in</MDBBtn>
+       <MDBBtn className="mb-4">Sign in</MDBBtn>
 
-    //   <div className="text-center">
-    //     <p>Not a member? <a href="#!">Register</a></p>
-    //     <p>or sign up with:</p>
+       <div className="text-center">
+         <p>Not a member? <a href="#!">Register</a></p>
+         <p>or sign up with:</p>
 
-    //     <div className='d-flex justify-content-between mx-auto' style={{width: '40%'}}>
-    //       <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-    //         <MDBIcon fab icon='facebook-f' size="sm"/>
-    //       </MDBBtn>
+         <div className='d-flex justify-content-between mx-auto' style={{width: '40%'}}>
+           <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+             <MDBIcon fab icon='facebook-f' size="sm"/>
+           </MDBBtn>
 
-    //       <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-    //         <MDBIcon fab icon='twitter' size="sm"/>
-    //       </MDBBtn>
+           <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+             <MDBIcon fab icon='twitter' size="sm"/>
+           </MDBBtn>
 
-    //       <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-    //         <MDBIcon fab icon='google' size="sm"/>
-    //       </MDBBtn>
+           <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+             <MDBIcon fab icon='google' size="sm"/>
+           </MDBBtn>
 
-    //       <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-    //         <MDBIcon fab icon='github' size="sm"/>
-    //       </MDBBtn>
+           <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+             <MDBIcon fab icon='github' size="sm"/>
+           </MDBBtn>
 
-    //     </div>
-    //   </div>
+         </div>
+       </div>
 
-    // </MDBContainer>
+     </MDBContainer>*/
+
+    
   );
 }
 
