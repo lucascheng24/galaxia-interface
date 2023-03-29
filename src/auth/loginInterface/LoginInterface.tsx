ff01f7bf2c6fff7952b4f14f47e44559639f6f3c
lucascheng24 <74233981+lucascheng24@users.noreply.github.com>
2023-03-29 21:23:43 +0800

    update input field of login & register

--- src/auth/loginInterface/LoginInterface.tsx
+++ src/auth/loginInterface/LoginInterface.tsx
@@ -13,19 +13,27 @@ import { LOGININTERFACE_PAGEMODE_TYPE } from './LoginInterfaceType';
 interface stateInterface {
   page: string,
   page_mode: LOGININTERFACE_PAGEMODE_TYPE,
+  loginInput: {
+    emailAddress?: string,
+    password?: string,
+    rePassword?: string
+  }
 
 }
 
 //  define initial state
 const initialState: stateInterface = {
   page: 'Login Interface',
-  page_mode: LOGININTERFACE_PAGEMODE_TYPE.default
+  page_mode: LOGININTERFACE_PAGEMODE_TYPE.default,
+  loginInput: {},
+
 }
 
 //  define action type
 const enum actionType {
   CHANGE_PAGE,
   CHANGE_PAGE_MODE,
+  ONCHANGE_LOGININPUT,
 }
 
 //  define action class
@@ -35,6 +43,10 @@ type actionClass = {
 } | {
   type: actionType.CHANGE_PAGE_MODE,
   value: LOGININTERFACE_PAGEMODE_TYPE //  page_mode
+} | {
+  type: actionType.ONCHANGE_LOGININPUT,
+  key: keyof stateInterface['loginInput'] ,
+  value?: string
 }
 
 //  reducer to handle state by different actions
@@ -46,6 +58,14 @@ const reducer = (state = initialState, action: actionClass) => {
       return {
         ...state,
         page_mode: action.value
+      }
+    case actionType.ONCHANGE_LOGININPUT:
+      return {
+        ...state,
+        loginInput: {
+          ...state.loginInput,
+          [action.key]: action.value
+        }
       }
     default:
       return state;
@@ -91,17 +111,89 @@ const LoginInterface = (props: any) => {
               sign in and start connect with friends
             </Grid>
             <Grid>
-              <TextField id="outlined-basic" label="Email address" variant="outlined" />
-              <TextField type={'password'} id="outlined-basic" label="Password" variant="outlined" />
+              <TextField 
+                id="outlined-basic" 
+                label="Email address" 
+                variant="outlined" 
+                value={state.loginInput.emailAddress}
+                onChange = {(e) => dispatch({ type: actionType.ONCHANGE_LOGININPUT, key: 'emailAddress', value: e.target.value})}
+              />
+              <TextField  
+                id="outlined-basic" 
+                label="Password" 
+                variant="outlined"
+                type={'password'}
+                value={state.loginInput.password}
+                onChange = {(e) => dispatch({ type: actionType.ONCHANGE_LOGININPUT, key: 'password', value: e.target.value})}
+              />
             </Grid>
-            {/* <Button className='buttonLogin' 
-              onClick={() => dispatch({type: actionType.CHANGE_PAGE_MODE, value: LOGININTERFACE_PAGEMODE_TYPE.login})}
-            >Login</Button>
+            <Grid className='standard-small-font font-black'>
+              <div>email address: {state.loginInput.emailAddress} </div>
+              <div>password: {state.loginInput.password} </div>
+            </Grid>
+            <Grid font-black>
+              <Button className='buttonLogin-2' style={{'marginLeft': '4rem'}}
+                onClick={() => dispatch({type: actionType.CHANGE_PAGE_MODE, value: LOGININTERFACE_PAGEMODE_TYPE.login})}
+              >Login</Button>
 
-            <Link className='' to={''}
-              onClick={() => dispatch({ type: actionType.CHANGE_PAGE_MODE, value: LOGININTERFACE_PAGEMODE_TYPE.register })}
-            >Register</Link> */}
+              <Link className='standard-small-font' to={''}
+                onClick={() => dispatch({ type: actionType.CHANGE_PAGE_MODE, value: LOGININTERFACE_PAGEMODE_TYPE.register })}
+              >Register</Link>
+            </Grid>
+          </div>
+        }
 
+
+
+        {/* register UI  */}
+        { state.page_mode == LOGININTERFACE_PAGEMODE_TYPE.register &&
+          <div>
+            <Image src={logo} className="logo-2" alt="Logo"/>
+            <Grid>
+              Register
+            </Grid>
+            <Grid className='standard-small-font font-black'>
+              sign in and start connect with friends
+            </Grid>
+            <Grid>
+              <TextField 
+                id="outlined-basic" 
+                label="Email address" 
+                variant="outlined" 
+                value={state.loginInput.emailAddress}
+                onChange = {(e) => dispatch({ type: actionType.ONCHANGE_LOGININPUT, key: 'emailAddress', value: e.target.value})}
+              />
+              <TextField  
+                id="outlined-basic" 
+                label="Password" 
+                variant="outlined"
+                type={'password'}
+                value={state.loginInput.password}
+                onChange = {(e) => dispatch({ type: actionType.ONCHANGE_LOGININPUT, key: 'password', value: e.target.value})}
+              />
+              <TextField  
+                id="outlined-basic" 
+                label="Re-Password" 
+                variant="outlined"
+                type={'password'}
+                value={state.loginInput.rePassword}
+                onChange = {(e) => dispatch({ type: actionType.ONCHANGE_LOGININPUT, key: 'rePassword', value: e.target.value})}
+              />
+            </Grid>
+            <Grid className='standard-small-font font-black'>
+              <div>email address: {state.loginInput.emailAddress} </div>
+              <div>password: {state.loginInput.password} </div>
+              <div>re-password: {state.loginInput.rePassword} </div>
+            </Grid>
+            <Grid font-black>
+              <Button className='buttonLogin-2' style={{'marginLeft': '4rem'}}
+                onClick={() => dispatch({type: actionType.CHANGE_PAGE_MODE, value: LOGININTERFACE_PAGEMODE_TYPE.login})}
+              >Register</Button>
+
+              {/* <Link className='standard-small-font' to={''}
+                onClick={() => dispatch({ type: actionType.CHANGE_PAGE_MODE, value: LOGININTERFACE_PAGEMODE_TYPE.register })}
+              >Register</Link> */}
+            </Grid>
           </div>
         }
         
