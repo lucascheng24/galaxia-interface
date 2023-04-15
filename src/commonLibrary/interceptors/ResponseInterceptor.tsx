7cec107e53eacc501cf6e5efec983b322f1853f2
lucascheng24 <74233981+lucascheng24@users.noreply.github.com>
2023-04-15 21:54:50 +0800

    Update ResponseInterceptor.tsx
    fix 401 issue

--- src/commonLibrary/interceptors/ResponseInterceptor.tsx
+++ src/commonLibrary/interceptors/ResponseInterceptor.tsx
@@ -1,21 +1,35 @@
 import axios from "axios";
 import { FC, ReactNode } from "react";
+import { useAuth } from "../../auth/UserProfileContext";
+import Cookies from "universal-cookie";
+import { STORED_COOKIE_PATH } from "../cookieClass";
 
 interface Props {
     children: ReactNode;
 }
 
 export const ResponseInterceptor: FC<Props> = ({ children }) => {
+
+    const cookies = new Cookies();
+    const { userProfile, setUserProfile } = useAuth();
     
     axios.interceptors.response.use( res => {
         //  do something here
-
+        
 
         return res;
     }, (error) => {
         // handle request error
         
-        console.log("response error: ", JSON.stringify(error));
+        
+        console.log("response error: ", error);
+
+        if (error.response.status === 401) {
+            console.log('response status - 401: clear all session!!')
+            cookies.remove(STORED_COOKIE_PATH.JWT);
+            cookies.remove(STORED_COOKIE_PATH.User_Info);
+            setUserProfile({});
+        }
         
         return error
     });
